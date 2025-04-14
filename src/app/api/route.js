082a7ca5e65be7146580057725ed618672490e77
Charlton Shih <168577923+chewton2k@users.noreply.github.com>
2025-04-13 19:05:30 -0700

    deleted comments

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -9,10 +9,6 @@ export async function GET(req) {
 	try {
 		let data, error;
 
-		// ({ data, error } = await supabase
-		// 	.from('clubs')
-		// 	.select('*'));
-
 		// If there's an "id" provided, fetch specific data for that ID
 		if (name && name !== "") {
 		// Fetch a single club by id
@@ -31,10 +27,6 @@ export async function GET(req) {
 			.select('*'));
 		}
 
-
-		// const { data, error } = await supabase
-		// .from('clubs')
-		// .select('*') // or specific columns: 'id, name, created_at'
 		
 		if (error) {
 			console.error("Fetch error:", error)
@@ -53,47 +45,4 @@ export async function GET(req) {
   }
 
 
-  	//   const response = await fetch("https://sa.ucla.edu/RCO/Public/SearchOrganizations", {
-	// 	method: "POST",
-	//   });
-
-  	//   const response = await fetch("https://sa.ucla.edu/RCO/Public/SearchOrganizations", {
-	// 	method: "POST",
-	//   });
-		// // make data into the correct format for supabase
-		// const orgList = data.orgList;
-
-		// // Sanitize a single object by removing null characters
-		// function sanitizeObject(obj) {
-		// 	const sanitizedObj = {};
-		// 	for (const key in obj) {
-		// 	if (typeof obj[key] === "string") {
-		// 		// Remove null characters from strings
-		// 		sanitizedObj[key] = obj[key].replace(/\u0000/g, "");
-		// 	} else {
-		// 		sanitizedObj[key] = obj[key];
-		// 	}
-		// 	}
-		// 	return sanitizedObj;
-		// }
-		
-		// // Sanitize the entire orgList array
-		// const sanitizedOrgList = orgList.map(sanitizeObject);
-
-
-	// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-	// const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-	// if (!supabaseUrl || !supabaseKey) {
-	// 	console.error("Supabase URL or Service Role Key is missing.");
-	// 	return new Response(JSON.stringify({ error: "Server configuration error" }), { status: 500 });
-	// }
-
-
-  		// // Insert data into the Supabase database
-		// const { error } = await supabase.from("clubs").insert(sanitizedOrgList);
-
-		// if (error) {
-		// console.error("Error inserting data into Supabase:", error);
-		// 	return new Response(JSON.stringify({ error: "Failed to insert data into database" }), { status: 500 });
-		// }
+  	
