dd24440f85e0cc088e6d06a56dbae2fcb514cbe4
DiAndyW <fangwanggo@outlook.com>
2025-05-18 23:42:39 -0700

    added fonts, button styles, fixed search by category dropdown not closing on click

--- src/app/components/tagButton.jsx
+++ src/app/components/tagButton.jsx
@@ -2,7 +2,7 @@
 export default function TagButton({ label, isSelected, onClick }) {
   return (
     <button
-      className={`rounded-full px-3 py-1 border ${isSelected ? 'bg-[#6E9461] text-white' : 'bg-[#CBDDC5]'
+      className={`rounded-full px-3 py-1 border ${isSelected ? 'bg-[#5086E1] text-white' : 'bg-[#EEF4FF] text-black'
         }`}
       onClick={onClick}
     >
