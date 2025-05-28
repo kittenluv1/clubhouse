beb5f27ca7f1a7f98fd7f62275e256d01d3b6135
kittenluv1 <ericahuang916@gmail.com>
2025-05-28 12:56:54 -0700

    tooltips & thankyou mobile

--- src/app/review/thankyou/page.js
+++ src/app/review/thankyou/page.js
@@ -2,11 +2,12 @@
 
 export default function ThankYouPage() {
   return (
-    <div className="flex flex-col items-center justify-center p-20">
-      <img
-        src="/empty-star-logo.svg"
-        alt="clubhouse star logo"
-        className="mb-8 w-1/4"
+    <div className="flex flex-col items-center justify-center p-20 text-center">
+      <object
+        type="image/svg+xml"
+        data="/empty-star-logo.svg"
+        aria-label="ClubHouse Logo"
+        className="mb-8 w-40 md:w-1/4"
       />
       <h1 className="mb-4 text-center text-6xl font-bold">Thank you!</h1>
       <p className="text-4x1 m-2 font-semibold">Your review has been sent!</p>
