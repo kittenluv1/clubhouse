d7c071b3addaededc7e5f0e40d4ad4818463e23c
kittenluv1 <ericahuang916@gmail.com>
2025-10-22 09:28:43 -0700

    footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,53 +1,77 @@
 "use client";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 
 export default function Footer() {
-  const pathname = usePathname();
+  // const pathname = usePathname();
 
   return (
     <>
-      {/* <div className="border"/> */}
-      <footer className="h-auto overflow-hidden bg-[#C4E1FC] p-15 break-words lg:px-30 lg:py-15">
-        <h3 className="mb-5 text-lg">
-          Have questions or feedback? Want to collaborate?
-        </h3>
-        <p>We’d love to hear from you!</p>
-        <p>
-          Whether you’re a student with suggestions, a club rep hoping to update
-          your listing, or just curious about how we verify reviews — reach out
-          anytime.
-        </p>
-        <br />
-        <a href="mailto:clubhouseucla@gmail.com" target="_blank">
-          <img src="/email.svg" alt="email icon" className="inline-block w-6" />
-          &nbsp;clubhouseucla@gmail.com
-        </a>
-        <br />
-        <a href="https://www.instagram.com/clubhouseucla/" target="_blank">
-          <img
-            src="/instagram.svg"
-            alt="instagram icon"
-            className="inline-block w-6"
-          />
-          &nbsp;@clubhouseucla
-        </a>
-        <br />
-        <a
-          href="https://www.linkedin.com/company/clubhouseucla/about/"
-          target="_blank"
-        >
-          <img
-            src="/linkedin.svg"
-            alt="linkedin icon"
-            className="inline-block w-6"
-          />
-          &nbsp;LinkedIn
-        </a>
-        <br />
-        <br />
-        <p>
-          Let’s build a more transparent, connected UCLA community — together.
-        </p>
+      <footer className="flex w-full flex-col gap-8 bg-[#C4E1FC] p-15 pt-20 break-words md:py-10 lg:flex-row lg:justify-between lg:gap-12 lg:px-20 lg:py-15">
+        {/* contact us */}
+        <div className="w-full flex-1 md:px-10 md:py-10 lg:max-w-120 lg:py-20 lg:pl-10">
+          <h3 className="mb-5 text-2xl font-bold">Contact Us</h3>
+          <p>
+            Have questions or feedback? Want to collaborate? The clubhouse
+            community would love to hear from you, we’re here to help!
+          </p>
+          <br />
+          <p>
+            Contact us at{" "}
+            <a
+              href="mailto:clubhouseucla@gmail.com"
+              target="_blank"
+              className="underline"
+            >
+              clubhouseucla@gmail.com
+            </a>
+          </p>
+        </div>
+        {/* connect and bear */}
+        {/* <div className="flex flex-1 flex-col justify-between md:flex-row"> */}
+        <div className="py-10 md:px-10 lg:py-20">
+          <h3 className="mb-5 text-2xl font-bold">Connect</h3>
+          <div className="grid auto-cols-max grid-flow-col gap-2">
+            <a href="https://www.instagram.com/clubhouseucla/" target="_blank">
+              <img
+                src="/instagram.svg"
+                alt="instagram icon"
+                className="inline-block w-8"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/clubhouseucla/about/"
+              target="_blank"
+            >
+              <img
+                src="/linkedin.svg"
+                alt="linkedin icon"
+                className="inline-block w-8"
+              />
+            </a>
+            <a href="https://www.tiktok.com/@clubhouseucla" target="_blank">
+              <img
+                src="/tiktok.svg"
+                alt="tiktok icon"
+                className="inline-block w-8"
+              />
+            </a>
+            <a
+              href="https://www.reddit.com/user/clubhouseucla/"
+              target="_blank"
+            >
+              <img
+                src="/reddit.svg"
+                alt="reddit icon"
+                className="inline-block w-8"
+              />
+            </a>
+          </div>
+        </div>
+        {/* bear */}
+        <div className="flex shrink-0 items-center justify-center md:py-10 lg:justify-end lg:p-0">
+          <img src="/bear.svg" alt="bear icon" />
+        </div>
+        {/* </div> */}
       </footer>
     </>
   );
