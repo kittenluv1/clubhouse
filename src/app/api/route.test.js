9d70d63e4ba643260f7ac45523046c293040fa66
Andy Wang <fangwanggo@outlook.com>
2026-04-21 12:08:46 -0700

    fix: fixed club test data not being reset correctly across tests

--- src/app/api/route.test.js
+++ src/app/api/route.test.js
@@ -16,56 +16,70 @@ jest.mock('../lib/server-db.js', () => ({
 	supabaseServer: mockSupabase
 }));
 
-// mock the SOLE API fetch
-global.fetch = jest.fn()
-	.mockResolvedValueOnce({
-		ok: true,
-		json: () => Promise.resolve({
-			orgList: [
-				{
-					"OrganizationID": 5731,
-					"OrganizationName": "Creative Labs UCLA",
-					"AdvisorName": "MIRANDA REDMAN",
-					"OrganizationEmail": "uclacreatives@gmail.com",
-					"OrganizationWebSite": "https://www.creativelabsucla.com/",
-					"Category1Name": "Technology",
-					"Category2Name": "Academic",
-					"MemberType": "Undergraduate",
-					"SocialMediaLink": "<a target='_blank' href='https://www.instagram.com/creativelabsucla/'>Instagram</a> , <a target='_blank' href='https://www.linkedin.com/company/creativelabs-la/people/'>Linkedin</a>"
-				},
-				{
-					OrganizationID: 6895,
-					OrganizationName: 'Longevity Club at UCLA',
-					AdvisorName: 'ORLANDO LUNA',
-					OrganizationEmail: 'uclalongevity@outlook.com',
-					OrganizationWebSite: '',
-					Category1Name: 'Health and Wellness',
-					Category2Name: 'Engineering',
-					MemberType: 'Graduate/Undergraduate',
-					SocialMediaLink: "<a target='_blank' href='https://www.instagram.com/uclalongevity'>Instagram</a>"
-				},
-			]
-		})
-	}).mockResolvedValueOnce({
-		ok: true,
-		json: () => Promise.resolve({
-			clubSportsOrgList: [
-				{
-					"OrganizationID": "V3Q2-L6L95",
-					"OrganizationName": "Archery",
-					"OrganizationDescription": "UCLA Archery is a beginner-friendly club open to all experience levels. We have a flexible practice schedule, allowing archers to dictate how much time and focus they dedicate to shooting. Our instructors will teach you everything you need to know and there are always experienced archers around to answer any questions or give you a helping hand. We attend tournaments in California as well as other states. All tournaments are open to everyone, regardless of skill level, but competing is a completely optional part of our club. For more information, feel free to check us out at the links below!",
-					"OrganizationEmail": "uclaclubarchery@gmail.com",
-					"OrganizationWebSite": "https://uclaclubsports.com/sports/archery",
-					"Category1Name": "Club Sports",
-					"Category2Name": "Archery",
-					"SocialMediaLink": "https://www.facebook.com/UCLAClubArchery/"
-				},
-			]
-		})
-	});
+// reusable mock responses
+const CLUBS_RESPONSE = {
+	ok: true,
+	json: () => Promise.resolve({
+		orgList: [
+			{
+				"OrganizationID": 5731,
+				"OrganizationName": "Creative Labs UCLA",
+				"AdvisorName": "MIRANDA REDMAN",
+				"OrganizationEmail": "uclacreatives@gmail.com",
+				"OrganizationWebSite": "https://www.creativelabsucla.com/",
+				"Category1Name": "Technology",
+				"Category2Name": "Academic",
+				"MemberType": "Undergraduate",
+				"SocialMediaLink": "<a target='_blank' href='https://www.instagram.com/creativelabsucla/'>Instagram</a> , <a target='_blank' href='https://www.linkedin.com/company/creativelabs-la/people/'>Linkedin</a>"
+			},
+			{
+				OrganizationID: 6895,
+				OrganizationName: 'Longevity Club at UCLA',
+				AdvisorName: 'ORLANDO LUNA',
+				OrganizationEmail: 'uclalongevity@outlook.com',
+				OrganizationWebSite: '',
+				Category1Name: 'Health and Wellness',
+				Category2Name: 'Engineering',
+				MemberType: 'Graduate/Undergraduate',
+				SocialMediaLink: "<a target='_blank' href='https://www.instagram.com/uclalongevity'>Instagram</a>"
+			},
+		]
+	})
+};
+
+const CLUB_SPORTS_RESPONSE = {
+	ok: true,
+	json: () => Promise.resolve({
+		clubSportsOrgList: [
+			{
+				"OrganizationID": "V3Q2-L6L95",
+				"OrganizationName": "Archery",
+				"OrganizationDescription": "UCLA Archery is a beginner-friendly club open to all experience levels. We have a flexible practice schedule, allowing archers to dictate how much time and focus they dedicate to shooting. Our instructors will teach you everything you need to know and there are always experienced archers around to answer any questions or give you a helping hand. We attend tournaments in California as well as other states. All tournaments are open to everyone, regardless of skill level, but competing is a completely optional part of our club. For more information, feel free to check us out at the links below!",
+				"OrganizationEmail": "uclaclubarchery@gmail.com",
+				"OrganizationWebSite": "https://uclaclubsports.com/sports/archery",
+				"Category1Name": "Club Sports",
+				"Category2Name": "Archery",
+				"SocialMediaLink": "https://www.facebook.com/UCLAClubArchery/"
+			},
+		]
+	})
+};
 
 describe('clubs API route: test fake fetch and update Supabase', () => {
 	const CREATIVE_LABS_ORG_ID = 5731;
+
+	// reset mocks and re-supply fetch responses before each test so they don't bleed between tests
+	beforeEach(() => {
+		jest.clearAllMocks();
+		mockSupabase.from.mockReturnThis();
+		mockSupabase.update.mockReturnThis();
+		mockSupabase.insert.mockReturnThis();
+		mockSupabase.eq.mockReturnThis();
+		mockSupabase.select.mockResolvedValue({ count: 1, error: null });
+		global.fetch = jest.fn()
+			.mockResolvedValueOnce(CLUBS_RESPONSE)
+			.mockResolvedValueOnce(CLUB_SPORTS_RESPONSE);
+	});
 
 	test('return 200 and club data on success', async () => {
 		const { GET } = await import('./route');
@@ -77,6 +91,8 @@ describe('clubs API route: test fake fetch and update Supabase', () => {
 	});
 
 	test('return 500 if fetch fails', async () => {
+		// override the beforeEach fetch mock with a failing response
+		fetch.mockReset();
 		fetch.mockResolvedValueOnce({ ok: false, status: 500 });
 		const { GET } = await import('./route');
 		const response = await GET(new NextRequest('http://localhost/api', { method: 'PATCH' }));
