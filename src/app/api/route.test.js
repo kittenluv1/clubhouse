1908b03dcf4cbb637e4eb637ae57c6e5fb573d63
Andy Wang <fangwanggo@outlook.com>
2026-04-22 18:37:52 -0700

    fix: fixed route test not passing since cache is not cleared between tests

--- src/app/api/route.test.js
+++ src/app/api/route.test.js
@@ -3,7 +3,6 @@
  */
 import { NextRequest } from 'next/server';
 
-// __mocks__/supabaseServer.js
 const mockSupabase = {
 	from: jest.fn().mockReturnThis(),
 	update: jest.fn().mockReturnThis(),
@@ -16,58 +15,66 @@ jest.mock('../lib/server-db.js', () => ({
 	supabaseServer: mockSupabase
 }));
 
-// mock the SOLE API fetch
-global.fetch = jest.fn()
-	.mockResolvedValueOnce({
-		ok: true,
-		json: () => Promise.resolve({
-			orgList: [
-				{
-					"OrganizationID": 5731,
-					"OrganizationName": "Creative Labs UCLA",
-					"AdvisorName": "MIRANDA REDMAN",
-					"OrganizationEmail": "uclacreatives@gmail.com",
-					"OrganizationWebSite": "https://www.creativelabsucla.com/",
-					"Category1Name": "Technology",
-					"Category2Name": "Academic",
-					"MemberType": "Undergraduate",
-					"SocialMediaLink": "<a target='_blank' href='https://www.instagram.com/creativelabsucla/'>Instagram</a> , <a target='_blank' href='https://www.linkedin.com/company/creativelabs-la/people/'>Linkedin</a>"
-				},
-				{
-					OrganizationID: 6895,
-					OrganizationName: 'Longevity Club at UCLA',
-					AdvisorName: 'ORLANDO LUNA',
-					OrganizationEmail: 'uclalongevity@outlook.com',
-					OrganizationWebSite: '',
-					Category1Name: 'Health and Wellness',
-					Category2Name: 'Engineering',
-					MemberType: 'Graduate/Undergraduate',
-					SocialMediaLink: "<a target='_blank' href='https://www.instagram.com/uclalongevity'>Instagram</a>"
-				},
-			]
+const mockOrgList = [
+	{
+		OrganizationID: 5731,
+		OrganizationName: 'Creative Labs UCLA',
+		AdvisorName: 'MIRANDA REDMAN',
+		OrganizationEmail: 'uclacreatives@gmail.com',
+		OrganizationWebSite: 'https://www.creativelabsucla.com/',
+		Category1Name: 'Technology',
+		Category2Name: 'Academic',
+		MemberType: 'Undergraduate',
+		SocialMediaLink: "<a target='_blank' href='https://www.instagram.com/creativelabsucla/'>Instagram</a> , <a target='_blank' href='https://www.linkedin.com/company/creativelabs-la/people/'>Linkedin</a>"
+	},
+	{
+		OrganizationID: 6895,
+		OrganizationName: 'Longevity Club at UCLA',
+		AdvisorName: 'ORLANDO LUNA',
+		OrganizationEmail: 'uclalongevity@outlook.com',
+		OrganizationWebSite: '',
+		Category1Name: 'Health and Wellness',
+		Category2Name: 'Engineering',
+		MemberType: 'Graduate/Undergraduate',
+		SocialMediaLink: "<a target='_blank' href='https://www.instagram.com/uclalongevity'>Instagram</a>"
+	},
+];
+
+const mockClubSportsOrgList = [
+	{
+		OrganizationID: 'V3Q2-L6L95',
+		OrganizationName: 'Archery',
+		OrganizationDescription: 'UCLA Archery is a beginner-friendly club open to all experience levels. We have a flexible practice schedule, allowing archers to dictate how much time and focus they dedicate to shooting. Our instructors will teach you everything you need to know and there are always experienced archers around to answer any questions or give you a helping hand. We attend tournaments in California as well as other states. All tournaments are open to everyone, regardless of skill level, but competing is a completely optional part of our club. For more information, feel free to check us out at the links below!',
+		OrganizationEmail: 'uclaclubarchery@gmail.com',
+		OrganizationWebSite: 'https://uclaclubsports.com/sports/archery',
+		Category1Name: 'Club Sports',
+		Category2Name: 'Archery',
+		SocialMediaLink: 'https://www.facebook.com/UCLAClubArchery/'
+	},
+];
+
+function mockSuccessFetches() {
+	fetch
+		.mockResolvedValueOnce({
+			ok: true,
+			json: () => Promise.resolve({ orgList: mockOrgList }),
 		})
-	}).mockResolvedValueOnce({
-		ok: true,
-		json: () => Promise.resolve({
-			clubSportsOrgList: [
-				{
-					"OrganizationID": "V3Q2-L6L95",
-					"OrganizationName": "Archery",
-					"OrganizationDescription": "UCLA Archery is a beginner-friendly club open to all experience levels. We have a flexible practice schedule, allowing archers to dictate how much time and focus they dedicate to shooting. Our instructors will teach you everything you need to know and there are always experienced archers around to answer any questions or give you a helping hand. We attend tournaments in California as well as other states. All tournaments are open to everyone, regardless of skill level, but competing is a completely optional part of our club. For more information, feel free to check us out at the links below!",
-					"OrganizationEmail": "uclaclubarchery@gmail.com",
-					"OrganizationWebSite": "https://uclaclubsports.com/sports/archery",
-					"Category1Name": "Club Sports",
-					"Category2Name": "Archery",
-					"SocialMediaLink": "https://www.facebook.com/UCLAClubArchery/"
-				},
-			]
-		})
-	});
+		.mockResolvedValueOnce({
+			ok: true,
+			json: () => Promise.resolve({ clubSportsOrgList: mockClubSportsOrgList }),
+		});
+}
 
 describe('clubs API route: test fake fetch and update Supabase', () => {
 	const CREATIVE_LABS_ORG_ID = 5731;
 
+	beforeEach(() => {
+		jest.clearAllMocks();
+		global.fetch = jest.fn();
+	});
+
 	test('return 200 and club data on success', async () => {
+		mockSuccessFetches();
 		const { GET } = await import('./route');
 		const response = await GET(new NextRequest('http://localhost/api', { method: 'PATCH' }));
 		const data = await response.json();
@@ -77,13 +84,15 @@ describe('clubs API route: test fake fetch and update Supabase', () => {
 	});
 
 	test('return 500 if fetch fails', async () => {
+		jest.spyOn(console, 'error').mockImplementation(() => {});
 		fetch.mockResolvedValueOnce({ ok: false, status: 500 });
 		const { GET } = await import('./route');
 		const response = await GET(new NextRequest('http://localhost/api', { method: 'PATCH' }));
 		expect(response.status).toBe(500);
 	});
 
-	test('don\'t include null club descriptions in update calls', async () => {
+	test("don't include null club descriptions in update calls", async () => {
+		mockSuccessFetches();
 		const { GET } = await import('./route');
 		await GET(new NextRequest('http://localhost/api'));
 
